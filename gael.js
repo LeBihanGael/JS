42f6c800bbc0533ea52279a4af0a2a9de1fe8d2f
LeBihanGael <gael.lebihan1606@gmail.com>
2025-11-28 10:43:47 +0100

    commit

---
+++ gael.js
@@ -0,0 +1,18 @@
+// declarer une variable
+var myVar = "Hello World";
+var myVar2 = 42;
+// declarer une variable locale
+let myVar3 = "je suis locale";
+// const permet de declarer une constante
+const myConst = 3.14;
+console.log(myVar);
+console.log(myVar2);
+console.log("Etape 1");
+
+function maFonction(paramertre) {
+    console.log("Je suis dans la fonction");
+    console.log(paramertre);
+}
+maFonction("Mon argument");
+
+
